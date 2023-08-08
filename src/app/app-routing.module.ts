85b6d82188a503cf0bfd5371fbfc8ff37cd186d4
Viswa184 <125149842+Viswa184@users.noreply.github.com>
2023-08-08 08:05:02 +0000

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  {path:'Login',component:LoginComponent},
+  {path:'Dashboard',component:DashboardComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router,Route } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Home Page';
+
+  constructor(private router:Router){
+
+  }
+  dashboardClick(){
+    this.router.navigate(['Dashboard']);
+  }
+
+  loginClick(){
+    this.router.navigate(['Login']);
+  }
+}
